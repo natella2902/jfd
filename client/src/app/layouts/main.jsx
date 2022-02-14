773c7d2d3c27147df69d9306a821a33a36c8c886
natella2902 <n.jelonkina@mail.ru>
2022-02-14 09:05:30 +0300

    fix bug with symbol

--- client/src/app/layouts/main.jsx
+++ client/src/app/layouts/main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
             <h1> Main Page</h1>
             <h3>Hello my dear fiends!</h3>
             <ul>
-                <li>This is my education project! Please don't hesitate registration form.</li>
+                <li>This is my education project! Please do not hesitate registration form.</li>
                 <li> It is absolutely safe. The project was done using MongoDB, React.js, Node.js</li>
             </ul>
         </div>
